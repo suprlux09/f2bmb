1f85fc31d36e92de65ea3a95b5df0c4f6248d33b
Sungcheol Heo <suprlux09@gmail.com>
2024-08-08 16:28:02 +0900

    receiver feat: 전송된 로그파일를 열어서 파싱 후 telegram bot으로 전송해 주는 로직 구현

---
+++ receiver/notify.py
@@ -0,0 +1,30 @@
+import asyncio
+import os
+import re
+from dotenv import load_dotenv
+from telegram import Bot
+
+# Load environment variables
+load_dotenv()
+token = os.getenv("TELEGRAM_BOT_TOKEN")
+userid = os.getenv("TELEGRAM_USER_ID")
+
+# Ensure the token and user ID are available
+if not token or not userid:
+    raise ValueError("Missing TELEGRAM_BOT_TOKEN or TELEGRAM_USER_ID in the environment variables.")
+
+async def main():
+    bot = Bot(token=token)
+
+    # get every file in the directory
+    for hostname in os.listdir("./logfiles"):
+        for line in open("./logfiles/" + hostname):
+            time = re.findall(r"^\d+\-\d+\-\d+\s\d+\:\d+\:\d+", line)[0]
+            service = re.findall(r"\[.*\]", line)[0][1:-1]
+            ip = re.findall(r"\d+\.\d+\.\d+\.\d+$", line)[0]
+
+            # send message to telegram
+            await bot.send_message(chat_id=userid, text=f"Time: {time}\nService: {service}\nIP: {ip}")
+        
+if __name__ == '__main__':
+    asyncio.run(main())
